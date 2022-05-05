0652cbd06188c2b4495fbf14145cd4ab68668cfb
Xlib <64978693+xlibraries@users.noreply.github.com>
2022-05-05 17:35:38 +0530

    Create  Maximum Subarray.cs

---
+++ Maximum Subarray.cs
@@ -0,0 +1,22 @@
+//Question Link:- https://leetcode.com/problems/maximum-subarray/submissions/
+public class Solution 
+{
+    public int MaxSubArray(int[] nums) 
+    {
+        int sum = 0;
+        int maxSum = nums[0];
+        for(int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+            if(nums[i] > sum)
+            {
+                sum = nums[i];
+            }
+            if(sum > maxSum)
+            {
+                maxSum = sum;
+            }
+        }
+        return maxSum;
+    }
+}
