ab9d821aeaf6377d1ecabeaa7a39ab2531ff4447
Xlib <64978693+xlibraries@users.noreply.github.com>
2022-05-06 16:19:08 +0530

    Create  Best Time to Buy and Sell Stock.cs

---
+++ Best Time to Buy and Sell Stock.cs
@@ -0,0 +1,21 @@
+//Question Link:-https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/
+public class Solution 
+{
+    public int MaxProfit(int[] prices) 
+    {
+        int minPrice = Int32.MaxValue;
+        int maxProfit = 0;
+        for(int i = 0; i < prices.Length; i++)
+        {
+            if(prices[i] < minPrice)
+            {
+                minPrice = prices[i];
+            }
+            else if(prices[i] - minPrice > maxProfit)
+            {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
