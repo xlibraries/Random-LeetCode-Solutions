a0ec8f540b7c98c77f803bcc3a9084f11aacd931
Xlib <64978693+xlibraries@users.noreply.github.com>
2022-05-14 16:36:39 +0530

    Create Diameter of a Binary Tree.cs

---
+++ Diameter of a Binary Tree.cs
@@ -0,0 +1,34 @@
+//Question Link:-https://leetcode.com/problems/diameter-of-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution 
+{
+   int max = 0;
+    
+    public int DiameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return max;
+    }
+    
+    private int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        
+        max = Math.Max(max, left + right);
+        
+        return Math.Max(left, right) + 1;
+    }
+}
