8a41fa2eabcada85b72039d68b6a932f8116362a
Xlib <64978693+xlibraries@users.noreply.github.com>
2022-12-23 03:26:19 -0800

    Create   Squares of a Sorted Array.cs

---
+++ Squares of a Sorted Array.cs
@@ -0,0 +1,12 @@
+public class Solution 
+{
+    public int[] SortedSquares(int[] nums) 
+    {
+        for(int i = 0; i < nums.Length; i++)
+        {
+            nums[i] *= nums[i];
+        }
+        Array.Sort(nums);
+        return nums;
+    }
+}
